e9c3f91060af7bd693f44ac9a8621e87cdb739a8
Logesh K <logeshkanna1408@gmail.com>
2025-12-03 23:15:05 +0530

    Add new utility and added new class Databasemanager class

---
+++ src/test/java/com/Inwarrenty/database/DemoJDBC.java
@@ -0,0 +1,33 @@
+package com.Inwarrenty.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DemoJDBC {
+	public static void main(String[] args) {
+		
+		DataBaseManager.CreateConnection();
+		DataBaseManager.CreateConnection();
+		DataBaseManager.CreateConnection();
+		DataBaseManager.CreateConnection();
+		
+		
+//		try {
+//			Connection conn = DriverManager.getConnection("jdbc:mysql://64.227.160.186:3306/SR_DEV", "srdev_ro_automation", "Srdev@123");
+//			java.sql.Statement statement = conn.createStatement();
+//		    ResultSet result= statement.executeQuery("select first_name, last_name ,mobile_number from tr_customer;");
+//		    while(result.next()) {
+//		    	String firstname = result.getString("first_name");
+//		    	String lastname = result.getString("last_name");
+//		    	String mobileno = result.getString("mobile_number");
+//		    	System.out.println(firstname +"| "+lastname +" | "+mobileno);
+//		    }
+//		} catch (SQLException e) {
+//			
+//			e.printStackTrace();
+//		}
+	}
+
+}
