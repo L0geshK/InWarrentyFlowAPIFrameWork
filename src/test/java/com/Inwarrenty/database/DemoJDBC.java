e114468f04156de4fb0964b812d304ab2a270611
Logeshkanna V <logeshkanna1408@gmail.com>
2025-12-07 23:32:07 +0530

    we added new java library  hikaricp also we added new two class first one is we craate db connection class with databsemanagerwithhikaricp

--- src/test/java/com/Inwarrenty/database/DemoJDBC.java
+++ src/test/java/com/Inwarrenty/database/DemoJDBC.java
@@ -6,14 +6,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DemoJDBC {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
+//		DataBaseManager.CreateConnection();
+//		DataBaseManager.CreateConnection();
+//		DataBaseManager.CreateConnection();
 		DataBaseManager.CreateConnection();
-		DataBaseManager.CreateConnection();
-		DataBaseManager.CreateConnection();
-		DataBaseManager.CreateConnection();
-		
-		
+		 Connection conn=  DataBaseManagerWithHikariCP.getConnecction();
+		System.out.println(conn);
 //		try {
 //			Connection conn = DriverManager.getConnection("jdbc:mysql://64.227.160.186:3306/SR_DEV", "srdev_ro_automation", "Srdev@123");
 //			java.sql.Statement statement = conn.createStatement();
