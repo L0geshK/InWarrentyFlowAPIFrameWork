822c19958aeb4e825e3fa4af8b08066ddb3276e6
Logeshkanna V <logeshkanna1408@gmail.com>
2025-12-15 22:28:16 +0530

    add new class for db connection open and get the data from db and we create a new dataprovider for createjob api for db

--- src/test/java/com/Inwarrenty/database/DemoJDBC.java
+++ src/test/java/com/Inwarrenty/database/DemoJDBC.java
@@ -1,9 +1,13 @@
 package com.Inwarrenty.database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Inwarrenty.Utils.CreateJobBeanMapper;
+import com.Inwarrenty.database.dao.CreateJobPayloadDataDao;
+import com.Inwarrenty.dataproviderbean.CreateJobBean;
+import com.Inwarrenty.request.model.CreateJobAPIPayload;
 
 public class DemoJDBC {
 	public static void main(String[] args) throws SQLException {
@@ -11,9 +15,9 @@ public class DemoJDBC {
 //		DataBaseManager.CreateConnection();
 //		DataBaseManager.CreateConnection();
 //		DataBaseManager.CreateConnection();
-		DataBaseManager.CreateConnection();
-		 Connection conn=  DataBaseManagerWithHikariCP.getConnecction();
-		System.out.println(conn);
+//		DataBaseManager.CreateConnection();
+//		 Connection conn=  DataBaseManagerWithHikariCP.getConnecction();
+//		System.out.println(conn);
 //		try {
 //			Connection conn = DriverManager.getConnection("jdbc:mysql://64.227.160.186:3306/SR_DEV", "srdev_ro_automation", "Srdev@123");
 //			java.sql.Statement statement = conn.createStatement();
@@ -28,6 +32,19 @@ public class DemoJDBC {
 //			
 //			e.printStackTrace();
 //		}
+		
+	List<CreateJobBean> beanlist=	CreateJobPayloadDataDao.getCreatePayloadData();
+	List<CreateJobAPIPayload>payloadlist = new ArrayList<CreateJobAPIPayload>();
+	for(CreateJobBean createjob :beanlist) {
+		CreateJobAPIPayload payload = CreateJobBeanMapper.mapper(createjob);
+		payloadlist.add(payload);
+		
+	}
+	for(CreateJobAPIPayload payload :payloadlist) {
+		System.out.println(payload);
+	}
+		
+		
 	}
 
 }
---
+++ src/test/java/com/Inwarrenty/dataprovidertest/InwarrentyCreateAPIDBDataProviderTestWith.java
@@ -0,0 +1,33 @@
+package com.Inwarrenty.dataprovidertest;
+
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import com.Inwarrenty.Constants.Roles;
+import com.Inwarrenty.Utils.SpecUtils;
+import com.Inwarrenty.request.model.CreateJobAPIPayload;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class InwarrentyCreateAPIDBDataProviderTestWith {
+
+	@Test(description = "Verifying if the  Create api is able to Create InWarrenty Job ", groups = { "api",
+			"regression",
+			"smoke" ,"Faker"}, dataProvider = "CreateJobAPIDBDataProvider", dataProviderClass = com.Inwarrenty.dataprovider.DataProviderUtils.class)
+	public void createJobApi(CreateJobAPIPayload createJobAPIPayload) throws IOException {
+
+		given()
+
+				.spec(SpecUtils.getRequestSpecWithAuth(Roles.FD, createJobAPIPayload)).when().post("/job/create").then()
+				.spec(SpecUtils.responseSpec_OK())
+				.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("ResponceSchema/CreateAPIResponceSchema.json"))
+				.body("message", Matchers.equalTo("Job created successfully. ")).body("data", Matchers.notNullValue())
+				.body("data.job_number", Matchers.startsWith("JOB_"));
+
+	}
+
+}
